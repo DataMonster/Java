8f0458affec24b5691b99b79beda701efa94a8ef
Zihan Wang <wangzhkevin@gmail.com>
2014-02-18 23:58:54 -0500

    Update collections.java

--- interview/ctci/basic/collections.java
+++ interview/ctci/basic/collections.java
@@ -1,2 +1,5 @@
 //java has a lot of useful collection including the list, queue, map, etc...
-//list
+/* abstract collection: AbstractList, AbstractMap, AbstractQueue, AbstractSequentialList, AbstractSet
+interface: Collection, List, Queue, Comparator, ListIterator, RandomAccess, Deque, Map, Set, Enumeration, Map.Entry, 
+           SortedMap, EventListener, NavigableMap, SortedSet, Formattable, NavigableSet, Iterator, Observer
+
