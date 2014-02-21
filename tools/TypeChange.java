ee8f6e3de72a314bcd159e84afe4f6678d472e60
Zihan Wang <wangzhkevin@gmail.com>
2014-02-21 00:04:25 -0500

    Create TypeChange.java

---
+++ tools/TypeChange.java
@@ -0,0 +1,50 @@
+public class TypeChange {
+  public TypeChange() {
+  }
+  //change the string type to the int type
+  public static  int stringToInt(String intstr)
+  {
+    Integer integer;
+    integer = Integer.valueOf(intstr);
+    return integer.intValue();
+  }
+  //change int type to the string type
+  public static String intToString(int value)
+  {
+    Integer integer = new Integer(value);
+    return integer.toString();
+  }
+  //change the string type to the float type
+  public static  float stringToFloat(String floatstr)
+  {
+    Float floatee;
+    floatee = Float.valueOf(floatstr);
+    return floatee.floatValue();
+  }
+  //change the float type to the string type
+  public static String floatToString(float value)
+  {
+    Float floatee = new Float(value);
+    return floatee.toString();
+  }
+  //change the string type to the sqlDate type
+  public static java.sql.Date stringToDate(String dateStr)
+  {
+    return  java.sql.Date.valueOf(dateStr);
+  }
+  //change the sqlDate type to the string type
+  public static String dateToString(java.sql.Date datee)
+  {
+    return datee.toString();
+  }
+ 
+  public static void main(String[] args)
+  {
+    java.sql.Date day ;
+    day = TypeChange.stringToDate("2003-11-3");
+    String strday = TypeChange.dateToString(day);
+    System.out.println(strday);
+  }
+ 
+
+}
