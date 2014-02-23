d8a5d9d31aa86c8c93571bc354c666fd4fdd42f6
Zihan Wang <wangzhkevin@gmail.com>
2014-02-22 22:22:40 -0500

    Create FindNumPair.java

---
+++ interview/question/FindNumPair.java
@@ -0,0 +1,16 @@
+/*Find number of pairs such that x^y > y^x
+Given two arrays X[] and Y[] of positive integers, find number of pairs such that x^y > y^x where x is an element from X[] and y is an element from Y[].
+
+Examples:
+
+  Input: X[] = {2, 1, 6}, Y = {1, 5}
+  Output: 3 
+  // There are total 3 pairs where pow(x, y) is greater than pow(y, x)
+  // Pairs are (2, 1), (2, 5) and (6, 1)
+
+
+  Input: X[] = {10, 19, 18}, Y[] = {11, 15, 9};
+  Output: 2
+  // There are total 2 pairs where pow(x, y) is greater than pow(y, x)
+  // Pairs are (10, 11) and (10, 15)
+*/
