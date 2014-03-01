49ee1f7b4392da158c7d3ff73dac8d17a9c0a980
Zihan <wangzhkevin@gmail.com>
2014-03-01 11:52:59 -0500

    first commit 03/01/14

---
+++ Common/src/edu/java/common2/WrapperClass.java
@@ -0,0 +1,17 @@
+package edu.java.common2;
+
+public class WrapperClass {
+
+	public WrapperClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
