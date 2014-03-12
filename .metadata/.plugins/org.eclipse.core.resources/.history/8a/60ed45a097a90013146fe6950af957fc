a0388080c344de803bb6fe358d740db23a029bf1
Zihan <wangzhkevin@gmail.com>
2014-03-11 23:50:20 -0400

    3/11 9

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8a/60ed45a097a90013146fe6950af957fc
@@ -0,0 +1,13 @@
+package com.march.question3;
+
+public class DeleteSingle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- GfG/src/com/march/question3/DeleteSingle.java
+++ GfG/src/com/march/question3/DeleteSingle.java
@@ -1,13 +1,31 @@
 package com.march.question3;
 
+/* Given only a pointer to a node to be deleted in a singly linked list, how do you delete it?
+A simple solution is to traverse the linked list until you find the node you want to delete. 
+But this solution requires pointer to the head node which contradicts the problem statement.
+ */
+
 public class DeleteSingle {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+}
+
+class Node{
+	Object data;
+	Object next;
+	public Node(){
+		this.data = null;
+		this.next = null;
+	}
+	public Node(Object data, Object next){
+		this.data = data;
+		this.next = next;
+	}
+}
+
+class LList{
+	
 }
