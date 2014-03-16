5f25ae5db1ad2c65702d1045020d27a58357e120
Zihan <wangzhkevin@gmail.com>
2014-03-16 17:37:30 -0400

    3/16 2

--- CTCI/src/edu/java/chap2/Node.java
+++ CTCI/src/edu/java/chap2/Node.java
@@ -18,4 +18,20 @@ public class Node {
 		n.next = end;
 	}
 
+	Node deleteNode(Node head, int d){
+		Node n = head;
+		
+		if(n.data == d){
+			return head.next;
+		}
+		
+		while(n.next !=null){
+			if(n.next.data == d){
+				n.next = n.next.next;
+				return head;
+			}
+			n =n.next;
+		}
+		return head;
+	}
 }
