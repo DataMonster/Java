d9e31494b84c374073ee1bd8237af2679256f3ef
Zihan <wangzhkevin@gmail.com>
2014-03-16 21:46:24 -0400

    03/16

---
+++ CTCI/src/edu/java/chap2/Palindrome.java
@@ -0,0 +1,12 @@
+package edu.java.chap2;
+
+//implement a function to check if a linked list is a palindrome
+public class Palindrome {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
