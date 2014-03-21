87694d4472c9471b771f30a4a48ddf546d6fe1d9
Zihan Wang <wangzhkevin@gmail.com>
2014-03-21 08:05:19 -0400

    Create SortStack.java

---
+++ CTCI/src/edu/java/chap3/SortStack.java
@@ -0,0 +1,4 @@
+/* Write a program to sort a stack in ascending order You should not make any assumptions about how the stack is 
+implemented The following are the only functions that should be used to write this program: push | pop | peek | isEmpty
+ */
+ 
