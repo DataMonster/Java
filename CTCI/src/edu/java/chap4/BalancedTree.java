4073293bdcf0a27949de36a069c6135ae1b7a0fb
Zihan Wang <wangzhkevin@gmail.com>
2014-03-21 17:12:19 -0400

    Create BalancedTree.java

---
+++ CTCI/src/edu/java/chap4/BalancedTree.java
@@ -0,0 +1,17 @@
+public static int maxDepth(TreeNode root) {
+  if(root == null) {
+    return 0;
+  }
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+
+public static int minDepth(TreeNode root) {
+  if (root == null) {
+    return 0;
+  }
+  return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+}
+
+public static boolean isBalanced(TreeNode root) {
+  return (maxDepth(root) - minDepth(root) <= 1);
+}
