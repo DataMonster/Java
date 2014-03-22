6e70e41e486734eb566a38d24a09c21b2b96e59c
Zihan <wangzhkevin@gmail.com>
2014-03-22 16:51:49 -0400

    03/22 12

---
+++ CTCI/src/edu/java/chap5/SwapOddEven.java
@@ -0,0 +1,18 @@
+package edu.java.chap5;
+
+/*
+ * write a program to swap odd and even bits in an integer with as few instructions as possible
+ * (e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped and so on.)
+ */
+
+public class SwapOddEven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
