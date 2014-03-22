face5f6a0ac8fd4c93d1c76d66060705f27c679d
Zihan <wangzhkevin@gmail.com>
2014-03-22 16:39:12 -0400

    03/22 10

---
+++ CTCI/src/edu/java/chap5/CovertBit.java
@@ -0,0 +1,20 @@
+package edu.java.chap5;
+
+/*
+ * write a function to determine the number of bits required to convert integer A to integer B
+ * input : 31, 14
+ * output: 2
+ * 
+ */
+
+public class CovertBit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CTCI/src/edu/java/moderate/TicTacToe.java
@@ -0,0 +1,15 @@
+package edu.java.moderate;
+
+//Design an algorithm to figure out if someone has wo a game of tic-tac-toe.
+
+public class TicTacToe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
