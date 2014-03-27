f837391a8e6a50755f9f072f93d6c649ea76b9fc
Zihan <wangzhkevin@gmail.com>
2014-03-26 23:57:47 -0400

    03/26 03

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c0/e012fa2f61b500131da58c2d9d53d9de
@@ -0,0 +1,10 @@
+package edu.java.chap4;
+
+public class NextNode {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ CTCI/src/edu/java/chap4/FirstAncester.java
@@ -0,0 +1,17 @@
+package edu.java.chap4;
+
+/*
+ * Design an algorithm and write code to find the first common ancestor of two nodes in
+ *  a binary tree. Avoid storing additional nodes in a data structure. Note: this is
+ *  not necessarily a binary search tree.
+ */
+
+public class FirstAncester {
+		
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
