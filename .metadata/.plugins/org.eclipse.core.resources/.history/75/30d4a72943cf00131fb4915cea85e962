ad77cce7db2b884f1e0d76b89496bd87f959a49b
Zihan <wangzhkevin@gmail.com>
2014-04-28 22:37:42 -0400

    042803

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/75/30d4a72943cf00131fb4915cea85e962
@@ -0,0 +1,14 @@
+package edu.java.thread;
+
+//daemon thread will stop right after all thread stop, call isDaemon to check 
+public class Daemon {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Common/src/edu/java/thread/StopThread.java
@@ -0,0 +1,35 @@
+package edu.java.thread;
+
+public class StopThread {
+
+
+	public static void main(String[] args) throws InterruptedException {
+		CanStopRunner runner = new CanStopRunner();
+		Thread t = new Thread(runner);
+		t.start();
+		Thread.sleep(100);
+		runner.stop();
+	}
+
+}
+
+class CanStopRunner implements Runnable {
+	private boolean stop = false;
+	
+	public void run() {
+		while(!stop){ // the sign is for stop
+			System.out.println("thread is running...");
+			try {
+				Thread.sleep(500);
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	public void stop() {
+		stop = true;
+	}
+	
+}
